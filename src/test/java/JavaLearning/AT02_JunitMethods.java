7e69268808f2d4fee59f85fa9986923e64faea45
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-14 14:12:21 +1100

    java

---
+++ src/test/java/JavaLearning/AT02_JunitMethods.java
@@ -0,0 +1,14 @@
+package JavaLearning;
+
+import org.junit.Test;
+
+public class AT02_JunitMethods {
+    @Test
+    public void test01(){
+        System.out.println("This is method 1");
+    }
+    @Test
+    public void test02(){
+        System.out.println("This is method 2");
+    }
+}
