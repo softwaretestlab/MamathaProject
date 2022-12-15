575f1cdb68055499237ca38dd34e23e625eb3984
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-15 12:57:17 +1100

    array

---
+++ src/test/java/JavaLearning/AT07_staticmethod01.java
@@ -0,0 +1,19 @@
+package JavaLearning;
+
+import org.junit.Test;
+
+public class AT07_staticmethod01 {
+
+
+    @Test
+    public static void test11(){
+
+        System.out.println("This is a static method");
+    }
+
+    @Test
+    public  static void test111(){
+
+        System.out.println("This is not-static method");
+    }
+}
