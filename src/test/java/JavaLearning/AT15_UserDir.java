6d19f9b70b2a42bd6444b44d7982ebe18bd68294
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-20 12:05:16 +1100

    selenium

--- src/test/java/JavaLearning/AT15_UserDir.java
+++ src/test/java/JavaLearning/AT15_UserDir.java
@@ -6,5 +6,8 @@ public class AT15_UserDir {
     @Test
     public void UserDirectory(){
         System.out.println(System.getProperty("user.dir"));
+
+       // C:\Users\softw\IdeaProjects\MamthaProject_STL\src\test\java\screenshots
+        System.out.println(System.getProperty("user.dir")+"//screenshots//capture-1.jpeg");
     }
 }
